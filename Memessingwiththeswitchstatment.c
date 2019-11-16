9ebd3593038d7b629ca23ebd48fdf255e23b585d
Djcoldcrown <Coldcrown370@gmail.com>
2019-11-15 19:30:02 -0500

    Create Memessingwiththeswitchstatment.c

---
+++ Memessingwiththeswitchstatment.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int num = 1;
+
+	switch (num) {
+		case 1:
+			printf("One\n");
+			break; 
+		case 2:
+			printf("Two\n");
+			break; 
+		case 3:
+			printf("Three\n");
+			break; 
+		case 4:
+		    printf("four\n");
+		    break;
+	    case 5:
+	       printf("five\n");
+	       break;
+		default:
+			printf("this number is above five i didnt code that yet\n");
+	}
+}
