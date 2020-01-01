34a9e9bb1ee7004d374154cd8cd8a94ffd547f8b
Djcoldcrown <Coldcrown370@gmail.com>
2019-12-31 22:39:21 -0500

    Create Numtotext.py

---
+++ Numtotext.py
@@ -0,0 +1,23 @@
+num = int(input("Enter a number in number form >>>"))
+if num == 1
+   print ("one")
+elif num == 2
+   print ("two")
+elif num == 3
+   print ("three")
+elif num == 4
+   print ("four")
+elif num == 5
+   print ("five")
+elif num == 6
+   print ("six")
+elif num == 7
+   print ("seven")
+elif num == 8
+   print ("eight")
+elif num == 9
+   print ("nine")
+eilf num == 10
+   print ("ten")
+else
+   print ("please enter a number between 1 and 10")
